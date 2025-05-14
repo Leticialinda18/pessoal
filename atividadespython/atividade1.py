8ea2b89c2c20f37f8baae18eca51c8365870ca33
Leticialinda18 <leticia_mariano@estudante.sesisenai.org.br>
2025-05-14 12:18:46 +0000

    update

--- atividadespython/atividade1.py
+++ atividadespython/atividade1.py
@@ -1,18 +1,11 @@
 
-# Peso dos ingredientes em gramas
-peso_queijo = 50
-peso_presunto = 50
-peso_hamburguer = 100
+quantidade = int(input("Insira a quantidade de sandwiches que deseja fazer: "))
 
-# Solicitar a quantidade de sanduíches
-quantidade_sanduiches = int(input("Digite a quantidade de sanduíches a fazer: "))
+presunto_queijo = 0.05
+hamburguer = 0.1
 
-# Calcular a quantidade total de cada ingrediente necessário em quilos
-total_queijo = (2 * peso_queijo * quantidade_sanduiches) / 1000  # kg
-total_presunto = (peso_presunto * quantidade_sanduiches) / 1000    # kg
-total_hamburguer = (peso_hamburguer * quantidade_sanduiches) / 1000 # kg
+calculo = quantidade * presunto_queijo
+calculo2 = quantidade * hamburguer
 
-# Exibir os resultados
-print(f"Quantidade de queijo necessária: {total_queijo:.2f} kg")
-print(f"Quantidade de presunto necessária: {total_presunto:.2f} kg")
-print(f"Quantidade de hambúrguer necessária: {total_hamburguer:.2f} kg")
+print(f"A quantidade para comprar de hamburguer é: {calculo2}Kg")
+print(f"A quantidade para comprar de presunto e queijo é {calculo}Kg")
